d615a134398ebfa88130bedfc465af56a76c8bbf
Arsheen Kour <117564092+arsheenkour@users.noreply.github.com>
2024-06-09 19:28:21 +0530

    Update reversingthenumbers.cpp

---
+++ reversingthenumbers.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+
+int a,b,c,d,e;
+cout<<"give 5 integers as input"<<endl;
+cin>>a>>b>>c>>d>>e;
+cout<<"the integers in reverse order are: "<<e<<d<<c<<b<<a;
+return 0;
+}
